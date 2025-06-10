d96fea739f1ff01186d753894b3e75a7944f6f62
LuizSouzaSP <luiz.hsouza@sptech.school>
2025-06-10 13:51:20 -0300

    cadastro dosfuncionarios funcionando

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -36,7 +36,50 @@ function autenticar(req, res) {
         }
     )
 }
+function cadastrar(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var nome = req.body.nomeServer;
+    var senha = req.body.senhaServer;
+    var email = req.body.emailServer;
+    var fkEmpresa = req.body.fkEmpresaServer;
+    var ativo = req.body.ativoServer;
+    var isAdmin = req.body.isAdminServer;
+
+    // Faça as validações dos valores
+    if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else if (fkEmpresa == undefined) {
+        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (ativo == undefined) {
+        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (isAdmin == undefined) {
+        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else {
+
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrar(nome, senha, email, fkEmpresa, ativo, isAdmin)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
 
 module.exports = {
-    autenticar
+    autenticar,
+    cadastrar
 }
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -10,7 +10,19 @@ function autenticar(email, senha) {
     console.log("Executando a instrução SQL: \n " + instrucaoSql);
     return database.executar(instrucaoSql);
 }
+function cadastrar(nome, senha, email, fkEmpresa, ativo, isAdmin) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, senha, email, fkEmpresa, ativo, isAdmin);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO Funcionario (nome, senha, email, fkEmpresa, ativo, isAdmin) VALUES ('${nome}', '${senha}', '${email}','${fkEmpresa}', '${ativo}', '${isAdmin}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 
 module.exports = {
-    autenticar
+    autenticar,
+    cadastrar
 }
